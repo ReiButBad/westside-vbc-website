23f7c03260446b73d600f0bef7847a7fe229c9b3
ReiButBad <reibutbad@gmail.com>
2024-12-25 20:19:07 +0700

    added meta tags for SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,6 +24,13 @@
 <svelte:head>
 	<title>WestSide Volleyball Community</title>
 	<link rel="icon" type="image/x-icon" href={Favicon}>
+    <meta name="description" content="The next level volleyball community in West Surabaya">
+    <meta name="keywords" content="volleyball, voli, olahraga, surabaya, surabaya barat">
+
+    <meta property="og:title" content="WestSide Volleyball Community">
+    <meta property="og:description" content="The next level volleyball community in West Surabaya">
+    <meta property="og:image" content="https://raw.githubusercontent.com/ReiButBad/westside-vbc-website/refs/heads/master/src/lib/images/logo.webp">
+    <meta property="og:url" content="https://westsidevbc.top">
 </svelte:head>
 {@render children()}
 
