f972a0cfe8adf32bc874e9c521fd2af45a9004a9
ReiButBad <reibutbad@gmail.com>
2024-12-27 14:59:21 +0700

    fixed resizing issue for mobile and tablet and darkened shool image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
     import Court from "$lib/images/court.webp"
     import Group3Horizontal from "$lib/images/group3_horizontal.webp"
     import Group3 from "$lib/images/group3.webp"
+    import SekolahCiputra from "$lib/images/sekolah_ciputra_darken.webp"
 </script>
 
 <main>
@@ -21,24 +22,23 @@
             <picture class="hidden xl:block">
                 <source srcset="{SpikeBlocked}">
                 <img 
-                  class="overflow-hidden w-full xl:h-dvh object-cover" 
+                  class="overflow-hidden w-full xl:h-screen object-cover" 
                   src="{SpikeBlocked}" 
-                  alt="banner">
+                  alt="spike">
             </picture>
         </div>
         <!-- <div class="xl:hidden bg-primary"><Header/></div> -->
     </div>
-    <div class="w-full h-dvh flex justify-center items-center bg-secondary" id="home">
+    <div class="w-full h-screen flex justify-center items-center bg-secondary" id="home">
         <div class="w-full h-5/6 md:h-1/2 lg:w-2/3 lg:h-2/3 p-10">
             <span class="prose">
-                <h2 class="text-neutral text-center text-3xl md:text-5xl">Join The WestSide Volleyball Community!</h2>
-                <p class="text-neutral text-lg text-center lg:text-xl">Welcome to the WESTSIDE Volleyball Community! Join us as we bring the excitement of volleyball to life in a place where the sport is just beginning to take root. Our vibrant community gathers every weekend and occasionally on weekdays for friendly matches that welcome players of all skill levels. Whether you're looking to sharpen your skills, make new friends, or simply enjoy the thrill of the game, you'll find your place here!
+                <h1 class="text-neutral text-center text-3xl md:text-5xl">Welcome to the WestSide Volleyball Community</h1>
+                <p class="text-neutral text-lg text-center lg:text-xl">Join us as we bring the excitement of volleyball to life in a place where the sport is just beginning to take root. Our vibrant community gathers every weekend and occasionally on weekdays for friendly matches that welcome players of all skill levels. Whether you're looking to sharpen your skills, make new friends, or simply enjoy the thrill of the game, you'll find your place here!
                 </p>
-                <span class="w-full flex justify-center items-center"><a href="https://chat.whatsapp.com/IsYs9YcvnXO12uWsaoUW35" target="_blank" class="border-primary hover:bg-primary hover:text-secondary border-2 text-primary px-10 py-2"><button>Join Our WhatsApp Group!</button></a></span>
             </span>
         </div>
     </div>
-    <div class="flex w-full xl:h-dvh">
+    <div class="flex w-full xl:h-screen">
         <div class="w-full h-full">
             <picture class="brightness-100">
                 <source 
@@ -48,7 +48,7 @@
                     media="(max-width: 1024px)" 
                   srcset="{Group3}">
                 <img 
-                  class="overflow-hidden w-full lg:h-dvh object-cover" 
+                  class="overflow-hidden w-full lg:h-screen object-cover" 
                   src="{Group3Horizontal}"
                   alt="banner">
                 <div class="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-t to-secondary via-transparent from-neutral">
@@ -62,39 +62,58 @@
         </div>
     </div>
     <div class="flex flex-col w-full">
-        <div class="flex items-center md:h-screen bg-neutral">
-            <div class="w-full p-2 lg:p-20 h-full flex flex-col-reverse lg:flex-row justify-center items-center">
-                <img class="lg:h-full p-10 md:px-40 lg:px-0 lg:pr-20" src={Group1} alt="group">
+        <div class="flex items-center lg:h-screen bg-neutral">
+            <div class="w-full p-2 lg:p-20 h-full flex flex-col-reverse xl:flex-row justify-center items-center">
+                <img class="xl:h-full p-10 md:px-40 lg:px-48 xl:px-0 xl:pr-20" src={Group1} alt="group">
                 <span class="prose p-10">
-                    <h2 class="text-secondary text-3xl lg:text-4xl">Activities</h2>
+                    <h1 class="text-secondary text-3xl lg:text-4xl">Activities</h1>
                     <p class="text-secondary lg:text-lg">We have matches on weekends, usually on Saturdays and Sundays, and occasionally on weekdays depending on player availability. We also sometimes host post-match dinners to celebrate and appreciate our efforts.</p>
                 </span>
             </div>
         </div>
-        <div class="flex items-center lg:h-screen bg-primary">
-            <div class="w-full p-2 lg:p-20 h-full flex flex-col-reverse lg:flex-row-reverse justify-center items-center">
-                <img class="lg:h-full p-10 md:px-40 lg:px-0 lg:pl-20" src={Group2} alt="group">
+        <div class="flex items-center md:h-screen bg-primary">
+            <div class="w-full p-2 lg:p-20 h-full flex flex-col-reverse xl:flex-row justify-center items-center">
+                <img class="xl:h-full p-10 md:px-40 lg:px-48 xl:px-0 xl:pr-20" src={Group2} alt="group">
                 <span class="prose px-10 pt-10 lg:p-10">
-                    <h2 class="text-secondary text-3xl lg:text-4xl">Members</h2>
+                    <h1 class="text-secondary text-3xl lg:text-4xl">Members</h1>
                     <p class="text-secondary lg:text-lg">We offer our members the privilege of playing on Sekolah Ciputra's court for free during practices or matches. Additionally, free water is provided to ensure you stay hydrated during activities.</p>
                     <span class="w-full flex justify-center lg:justify-start"><a href="/" class="border-neutral bg-neutral hover:border-secondary hover:bg-secondary text-secondary hover:text-neutral border-2 px-10 py-2"><button>View Our Committee</button></a></span>
                 </span>
             </div>
         </div>
         <div class="flex items-center lg:h-screen bg-secondary">
-            <div class="w-full p-2 lg:p-20 h-full flex flex-col-reverse lg:flex-row justify-center items-center">
-                <img class="lg:h-full p-10 md:px-40 lg:px-0 lg:pr-20" src={Court} alt="court">
+            <div class="w-full p-2 lg:p-20 h-full flex flex-col-reverse xl:flex-row justify-center items-center">
+                <img class="xl:h-full p-10 md:px-40 lg:px-48 xl:px-0 xl:pr-20" src={Court} alt="court">
                 <span class="prose p-10">
-                    <h2 class="text-neutral text-3xl lg:text-4xl">Training</h2>
+                    <h1 class="text-neutral text-3xl lg:text-4xl">Training</h1>
                     <p class="text-neutral lg:text-lg">We hold training sessions on match days, allowing players to immediately apply what they've practiced to real gameplay. Our training doubles as a warm-up, focusing on building confidence and honing skills through hands-on experience. It's all about growing together as a team and leveling up through practice and play.</p>
                 </span>
             </div>
         </div>
-        <!-- <div class="w-full py-20 px-5">
-            <span class="prose text-secondary">
-                <p class="text-xl">We usually play in <a target="_blank" href="https://maps.app.goo.gl/94kQcQ2opjreAUyT6">Sekolah Ciputra Surabaya</a> for most of our matches. But we occasionally play in <a target="_blank" href="https://maps.app.goo.gl/wJFnHrA6AXUTVLEz9">SMP SMA Kristen Masa Depan Cerah</a>, so it depends on our members and our availablity. Even so, we still gurantee matches on weekend (except when its a national or international holiday)</p>
+    </div>
+    <div class="w-full h-screen">
+        <div class="flex w-full h-full p-5">
+            <div class="w-full h-full bg-center bg-fixed lg:!bg-cover" style="background-image: url('{SekolahCiputra}'); background-size: auto 100%;">
+                <div class="w-full h-full">
+                    <div class="flex justify-center items-center w-full h-full">
+                        <span class="prose text-center">
+                            <h1 class="text-secondary font-norwester font-normal text-6xl">LOCATION</h1>
+                            <span class="w-full flex justify-center lg:justify-start"><a target="_blank" href="https://maps.app.goo.gl/94kQcQ2opjreAUyT6" class="border-secondary hover:border-primary hover:bg-primary text-secondary hover:text-secondary border-2 px-10 py-2"><button>Sekolah Ciputra Surabaya</button></a></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-full h-screen flex justify-center items-center bg-secondary">
+        <div class="w-full md:h-1/2 lg:w-2/3 lg:h-2/3 p-10">
+            <span class="prose">
+                <h1 class="text-neutral text-center text-3xl md:text-5xl">Join The WestSide Volleyball Community!</h1>
+                <p class="text-neutral text-lg text-center lg:text-xl">Don't miss out on the action, be part of a growing volleyball community where the love for the sport is taking off! We gather every weekend and occasionally on weekdays for exciting matches that welcome players of all skill levels. It's your chance to improve your game, build lasting friendships, and experience the thrill of volleyball like never before. So, what are you waiting for? Join us now and let's hit the court together!
+                </p>
+                <span class="w-full flex justify-center items-center"><a href="https://chat.whatsapp.com/IsYs9YcvnXO12uWsaoUW35" target="_blank" class="border-primary hover:bg-primary hover:text-secondary border-2 text-primary px-10 py-2"><button>Join Our WhatsApp Group!</button></a></span>
             </span>
-        </div> -->
+        </div>
     </div>
 </main>
 
